7f56aac9c45e5f4e4ff39cfd8ed06971dfa8c2e9
Adnan Erlansyah <adnanerlansyah403@gmail.com>
2023-02-11 20:04:11 +0700

    created detailpage

---
+++ App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace ngajilagi;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace ngajilagi;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
